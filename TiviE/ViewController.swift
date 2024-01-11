564bb6eeca0ba1066101a1bfc7e86b996df55f62
ninhlk <ninhdtvt@gmail.com>
2024-01-11 13:00:19 +0700

    login screen

--- TiviE/ViewController.swift
+++ TiviE/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var yourAcc: UITextField!
+
+    @IBOutlet weak var signinButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func siginButtonClicked(_ sender: Any) {
+        
+        let text = yourAcc.text ?? ""
+        let emailAlert = UIAlertController(title: "Thông báo", message: "Email chưa được đăng ký", preferredStyle: .alert)
+        let actionOK = UIAlertAction(title: "OK", style: .default)
+        
+        if text == "ninhlk" {
+            self.performSegue(withIdentifier: "showHomeScreen", sender: nil)
+            return
+        } else {
+            emailAlert.addAction(actionOK)
+            self.present(emailAlert, animated: true)
+            return
+        }
+    }
+    
 }
 
---
+++ TiviE/ViewController_Home.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController-Home.swift
+//  TiviE
+//
+//  Created by Ninh Lê on 11/01/2024.
+//
+
+import UIKit
+
+class ViewController_Home: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
